ac9938e9a7cf61c1c00f33dd214a429d4b93fcc2
Naveen <prajapatinaveen9719@gmail.com>
2023-05-17 19:46:33 +0530

    add state in class based component

--- The_Chef/src/components/aboutUs.jsx
+++ The_Chef/src/components/aboutUs.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 
 class AboutUsComponent extends React.Component {
-
-
+constructor(){
+  super();
+this.state={
+  Counter:10
+}
+}
   render(){
     return(
+      <>
         <h1>This is about us page.</h1>
+        <h2>{this.state.Counter}</h2></>
     )
   }
 }
