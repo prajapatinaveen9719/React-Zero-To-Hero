1650ab6bc13f3d228979d5ff652ea8d6583df925
Naveen <prajapatinaveen9719@gmail.com>
2023-05-18 20:12:47 +0530

    use of componentWillMount in functionalcomponent

--- The_Chef/src/components/Contact.js
+++ The_Chef/src/components/Contact.js
@@ -1,6 +1,19 @@
+import { useEffect } from "react";
 import AboutUsComponent from "./aboutUs";
 
 const ContactUsComponent=()=>{
+
+    useEffect(()=>{
+     const timer=setInterval(() => {
+            console.log(' fn interval');
+        }, 1000);
+
+        return()=>{
+            console.log('interval clear')
+            clearInterval(timer)
+        }
+    })
+
     return(
         <>
         <AboutUsComponent/>
